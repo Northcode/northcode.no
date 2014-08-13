8a162f7260cfff061bbdffb89531b42bdc7eb6ba
Andreas Larsen <northcode.no@gmail.com>
2014-08-13 19:37:52 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title></title>
+    <link href="bs/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- <link href="bs/css/bootstrap-theme.min.css" rel="stylesheet" /> -->
+  </head>
+  <body>
+    <div class="navbar navbar-static-top navbar-default">
+      <div class="container">
+        <a class="navbar-brand">Northcode</a>
+      </div>
+    </div>
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Welcome to Northcode!</a>
+      </div>
+    </div>
+
+
+    <!-- LOAD SCRIPTS -->
+    <script src="js/jq.js" />
+  </body>
+</html>
---
+++ sql/mysqldata.php
@@ -0,0 +1,9 @@
+<?php
+
+$DB_HOST = "localhost";
+$DB_USER = "root";
+$DB_PASS = "1234";
+$DB_NAME = "northcode";
+
+
+?>
