364f95434de9e7d2bc520403279701dfeba8094a
Jens <jens@northcode.no>
2015-09-20 20:04:52 +0200

    Updated about us page

--- about.php
+++ about.php
@@ -8,17 +8,26 @@
     <?php include("res/header.php"); ?>
 
 			<div id="content" class="container">
-				<h2>About us:</h2>
-				<h4>Andreas Larsen</h4>
-				<p class="aboutblock">
-					Im a programmer for norway. I usually code in c#, c++ or do web design.
-					I do a lot of the server management for northcode, and host the server.
-				</p>
+				<div class="col-md-4">
+					<h2>Northcode</h2>
+					<p>Northcode is currently a team of two people, the cousins Andreas Larsen and Jens Vogler. We started out teaching programming our selves and then started making projects togheter. The kickstart to the cooperative work was the Minecraft server which we both maintained and then later the MC-Manger Application which we worked on togheter.</p>
+					<p>From there on we continued with our projects. Sometimes togheter and at other times we worked individually on stuff. But our bond persisted, which is still strong based on family and mutual interest.</p>
+				</div>
 
-				<h4>Jens Vogler</h4>
-				<p class="aboutblock">
-					Jens is a programmer for switzerland. He maintains <a href="http://cobaltvault.no">cobalt vault</a>, and usually codes in c#, python or web languages.
-				</p>
+				<div class="col-md-4">
+					<h2>Andreas Larsen</h2>
+					<p class="aboutblock">
+						I am 20 years old and a programmer from Norway. I usually code in c#, c++ or do web design.
+						I do a lot of the server management for northcode, and host the server.
+					</p>
+				</div>
+
+				<div class="col-md-4">
+					<h2>Jens Vogler</h2>
+					<p class="aboutblock">
+						I am 17 years old and a programmer from Switzerland. I maintain <a href="http://cobaltvault.no">CobaltVault</a>, and usually code in c#, python or web languages.
+					</p>
+				</div>
 			</div>
 
 			<?php include($_SERVER['DOCUMENT_ROOT'] . "/res/footer.php"); ?>
