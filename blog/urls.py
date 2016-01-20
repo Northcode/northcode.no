fd2f25062a56a0ada846658e7dde88cc8a294eb9
Andreas Larsen <northcode.no@gmail.com>
2016-01-20 02:06:05 +0100

    Added meta info and login functionality

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Index.as_view(), name='index'),
-    url(r'^login', views.login, name='login'),
+    url(r'^login', 'django.contrib.auth.views.login', { 'template_name': 'blog/login.html', 'redirect_field_name':'next' } , name='login'),
+    url(r'^logout', 'django.contrib.auth.views.logout', { 'next_page': 'index' } , name='logout'),
     url(r'^password_reset', views.password_reset, name='password_reset'),
-    url(r"^gallery/", views.gallery, name="gallery"),
 ]
