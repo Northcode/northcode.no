a6c1d0522df2c0ab3c5e9c790003ac06f0e9d163
Andreas Larsen <northcode.no@gmail.com>
2016-04-24 19:25:46 +0200

    Turned page into one-page style page...
    + Fixed deprecated urlpatterns scheme in blog for default views
    + Added gulpfile
    + Moved styling to sass file
    + Changed some styling
    
    Now to upload and spot all the errors with the validator, should be fun right?

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login, logout
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.Index.as_view(), name='index'),
-    url(r'^login', 'django.contrib.auth.views.login', { 'template_name': 'blog/login.html', 'redirect_field_name':'next' } , name='login'),
-    url(r'^logout', 'django.contrib.auth.views.logout', { 'next_page': 'index' } , name='logout'),
+    url(r'^login', login, { 'template_name': 'blog/login.html', 'redirect_field_name':'next' } , name='login'),
+    url(r'^logout', logout, { 'next_page': 'index' } , name='logout'),
 ]
