4800897ab635ddb2c3458a43c53c414089221389
Andreas Larsen <northcode.no@gmail.com>
2018-02-04 08:16:16 +0100

    Fancier editing of blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,9 @@ from . import feeds
 
 urlpatterns = [
     url(r'^$', views.Index.as_view(), name='index'),
+    url(r'^post/new$', views.new_post, name='new_post'),
     url(r'^post/(?P<pk>\d+)/$', views.BlogPost.as_view(), name='blogpost'),
+    url(r'^post/(?P<pk>\d+)/edit$', views.edit_post, name='edit_post'),
     url(r'^rss$', feeds.BlogFeed(), name='rssfeed'),
     url(r'^login', login, { 'template_name': 'blog/login.html', 'redirect_field_name':'next' } , name='login'),
     url(r'^logout', logout, { 'next_page': 'index' } , name='logout'),
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Post
+from .forms import PostForm
+
+
 
 # Create your views here.
 class Index(ListView):
-	template_name = 'blog/index.html'
-	context_object_name = 'blog_posts'
-	paginate_by = 5
+        template_name = 'blog/index.html'
+        context_object_name = 'blog_posts'
+        paginate_by = 5
 
-	def get_queryset(self):
-		return Post.objects.order_by('-posted')
+        def get_queryset(self):
+                return Post.objects.order_by('-posted')
 
 class BlogPost(DetailView):
-	model = Post
-	template_name = 'blog/post.html'
-	context_object_name = 'post'
+        model = Post
+        template_name = 'blog/post.html'
+        context_object_name = 'post'
+
+def save_post(form,user):
+        if form.is_valid():
+                post = form.save(commit=False)
+                post.author = user
+                post.save()
+
+                return redirect("blogpost", pk=post.pk)
+        else:
+                return HttpResponse(400, "no")
+
+def new_post(request):
+        if request.user.is_authenticated():
+            if request.method == "GET":
+                    form = PostForm()
+                    return render(request, "blog/new_post.html", {
+                            "form" : form
+                    })
+            else:
+                    return save_post(PostForm(request.POST), request.user)
+
+def edit_post(request, pk):
+        if request.user.is_authenticated():
+                if request.method == "GET":
+                        post = Post.objects.get(pk=pk)
+                        form = PostForm(instance=post)
+                        return render(request, "blog/new_post.html", { "form": form })
+                else:
+                        return save_post(PostForm(request.POST), request.user)
